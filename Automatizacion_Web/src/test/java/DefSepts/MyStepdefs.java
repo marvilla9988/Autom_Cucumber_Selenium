dd7a6d7faf0beef71322c68f639117be9e7581fb
marjorievillalobos <mar.villalobos9988@gmail.com>
2020-05-06 22:50:14 -0600

    Actualización con archvio .properties y DataTest

--- Automatizacion_Web/src/test/java/DefSepts/MyStepdefs.java
+++ Automatizacion_Web/src/test/java/DefSepts/MyStepdefs.java
@@ -1,31 +1,24 @@
 package DefSepts;
+/*Clase generada desde muestra.feature con la opcion de generar all steps definition Java(Java 8)*/
 
 import Paginas.GoogleSearchPage;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java8.En;
 
+public class MyStepdefs implements En {
 
-public class MyStepdefs {
-    GoogleSearchPage googleSearch = new GoogleSearchPage();
+    public MyStepdefs() {
+        GoogleSearchPage googleSearch = new GoogleSearchPage();
 
-    @Given("I am on the Google search page")
-    public void iAmOnTheGoogleSearchPage() {
-        googleSearch.navigateToGoogle();
-    }
-
-    @When("I enter a world in the search field")
-    public void iEnterAWorldInTheSearchField() {
-        googleSearch.enterSearchCriteria("www.google.com");
-    }
-
-    @And("Click on the search button")
-    public void clickOnTheSearchButton() {
-        googleSearch.clickOnSearchButtonGoogle();
-    }
-
-    @Then("I see results related to the word entered")
-    public void iSeeResultsRelatedToTheWordEntered() {
+        Given("^I am on the Google search page$", () -> {
+            googleSearch.navigateToGoogle();
+        });
+        When("^I enter a world in the search field$", () -> {
+            googleSearch.enterSearchCriteria("www.thefreerangetester.com");
+        });
+        And("^Click on the search button$", () -> {
+            googleSearch.clickOnSearchButtonGoogle();
+        });
+        Then("^I see results related to the word entered$", () -> {
+        });
     }
 }
--- Automatizacion_Web/src/test/java/Paginas/GoogleSearchPage.java
+++ Automatizacion_Web/src/test/java/Paginas/GoogleSearchPage.java
@@ -1,15 +1,19 @@
 package Paginas;
+import Config.PropertyManager;
+
 
 public class GoogleSearchPage extends BasePage {
     private String searchBox = "//input[@title='Buscar']";
     private String searchButton = "//div[@class='FPdoLc tfB0Bf']//input[@class='gNO89b']";
+    private String testData = PropertyManager.getInstance().getSearch_url();
 
     public GoogleSearchPage(){
         super(driver);
     }
 
+
     public void navigateToGoogle(){
-        navigateTo("https://www.google.com");
+        navigateTo(testData);
     }
 
     public void enterSearchCriteria(String searchText){
